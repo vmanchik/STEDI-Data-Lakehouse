193910a4b4ffb51fa8ca4937a4d26f36c2b56ccc
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-16 02:31:01 -0800

    Update step_trainer_trusted.py

--- step_trainer_trusted.py
+++ step_trainer_trusted.py
@@ -18,21 +18,28 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# Script generated for node customer_curated
+customer_curated_node1734343671582 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_curated", transformation_ctx="customer_curated_node1734343671582")
+
+# Script generated for node accelerometer_trusted
+accelerometer_trusted_node1734341296121 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_trusted", transformation_ctx="accelerometer_trusted_node1734341296121")
+
 # Script generated for node step_trainer_landing
 step_trainer_landing_node1734341260287 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_landing", transformation_ctx="step_trainer_landing_node1734341260287")
 
-# Script generated for node accelerometer_trusted
-accelerometer_trusted_node1734341296121 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_trused", transformation_ctx="accelerometer_trusted_node1734341296121")
-
 # Script generated for node SQL Query
 SqlQuery0 = '''
-select sensorReadingTime,serialNumber,distancefromobject
+select distinct sensorReadingTime,
+step_trainer_landing.serialNumber,
+distancefromobject
 
 from step_trainer_landing 
 join accelerometer_trusted
 on accelerometer_trusted.timestamp = step_trainer_landing.sensorReadingTime
+join customer_curated on customer_curated.serialNumber = step_trainer_landing.serialNumber
+and customer_curated.email = accelerometer_trusted.user
 '''
-SQLQuery_node1734341355821 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"accelerometer_trusted":accelerometer_trusted_node1734341296121, "step_trainer_landing":step_trainer_landing_node1734341260287}, transformation_ctx = "SQLQuery_node1734341355821")
+SQLQuery_node1734341355821 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"accelerometer_trusted":accelerometer_trusted_node1734341296121, "step_trainer_landing":step_trainer_landing_node1734341260287, "customer_curated":customer_curated_node1734343671582}, transformation_ctx = "SQLQuery_node1734341355821")
 
 # Script generated for node Amazon S3
 AmazonS3_node1734341548977 = glueContext.getSink(path="s3://udacity-stedi/step_trainer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1734341548977")
