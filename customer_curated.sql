633588c76db52a1462ff576c41eac3c6eaa34c43
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-16 01:06:56 -0800

    Create customer_curated.sql

---
+++ customer_curated.sql
@@ -0,0 +1,23 @@
+CREATE EXTERNAL TABLE `customer_curated`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://udacity-stedi/customer/curated/'
+TBLPROPERTIES (
+  'CreatedByJob'='customer_curated', 
+  'CreatedByJobRun'='jr_145bd8824c9d120d30fe87efac73d845c12b56992ccdeae3a35841e67dbd7415', 
+  'classification'='json')
